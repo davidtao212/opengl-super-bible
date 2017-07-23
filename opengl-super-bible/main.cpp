c05aade8c47134e4c0a0fe55d35085580b7901e1
chengtao <chengtao@kuaishou.com>
2017-07-23 19:10:34 +0800

    Set up env, add Chapter01 and Chapter02.

---
+++ opengl-super-bible/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  opengl-super-bible
+//
+//  Created by chengtao on 23/07/2017.
+//  Copyright © 2017 chengtao. All rights reserved.
+//
+
+#include "main.h"
+
+int main(int argc, char * argv[]) {
+  return Block::main(argc, argv);
+}
+
---
+++ opengl-super-bible/main.h
@@ -0,0 +1,30 @@
+//
+//  main.h
+//  opengl-super-bible
+//
+//  Created by chengtao on 23/07/2017.
+//  Copyright © 2017 chengtao. All rights reserved.
+//
+
+#ifndef main_h
+#define main_h
+
+// Chapter01
+namespace Block {
+  int main(int argc, char* argv[]);
+}
+
+// Chapter02
+namespace Bounce {
+  int main(int argc, char* argv[]);
+}
+
+namespace Move {
+  int main(int argc, char* argv[]);
+}
+
+namespace Triangle {
+  int main(int argc, char* argv[]);
+}
+
+#endif /* main_h */
